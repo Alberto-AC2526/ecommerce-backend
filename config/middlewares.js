de9a47fc2a3a84e0fd5cf7400ed6dabe3295aea9
albertogonzalez-ac2526 <albertogonzalez.ac2526@gmail.com>
2026-03-24 17:48:26 +0100

    Config CORS para Vercel

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,31 @@
+// config/middlewares.js
 module.exports = [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::errors",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": ["'self'", "data:", "blob:", "https://res.cloudinary.com"], // Si usas Cloudinary luego
+          "media-src": ["'self'", "data:", "blob:"],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  {
+    name: "strapi::cors",
+    config: {
+      origin: ["http://localhost:8080", "vue-js-ecommerce.vercel.app"],
+    },
+  },
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
